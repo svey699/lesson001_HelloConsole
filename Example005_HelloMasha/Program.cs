f99b3a6d073d6c00d7dcf4a23e4e1a87d46b7f8f
Olga <svey699@gmail.com>
2022-10-15 19:39:49 +0500

    if and else

---
+++ Example005_HelloMasha/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура! Это же Маша");
+}
+else
+{
+    Console.Write("Привет");
+}
