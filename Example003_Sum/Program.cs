2f06eea7c9823477df9734889863063fe8d05355
Olga <svey699@gmail.com>
2022-10-15 19:17:31 +0500

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int a = 3;
-int b = 8;
+﻿int a = new Random().Next(1, 10); // 1, 2, 3, 4...9
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
 Console.WriteLine(a + b);
