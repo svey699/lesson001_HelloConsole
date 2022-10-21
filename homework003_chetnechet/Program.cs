fca8254d3ccf99151becbe442af3aad498401b47
Olga <svey699@gmail.com>
2022-10-21 20:36:26 +0500

    четное и нечетное

---
+++ homework003_chetnechet/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+
+double a = 4;
+double b = -3;
+double c = 7;
+
+
+    if (a % 2 == 0) Console.WriteLine("Четное 4");
+    else Console.WriteLine("Нечетное");
+    if (b % 2 == 0) Console.WriteLine("Четное -3");
+    else Console.WriteLine("Нечетное");
+    if (c % 2 == 0) Console.WriteLine("Четное 7");
+    else Console.WriteLine("Нечетное");
