3251f1ef0bb1e4ac3a0330f5d46c97c1a8628d35
Olga <svey699@gmail.com>
2022-10-22 21:26:48 +0500

    исправлены косяки

--- homework001_maxmin/Program.cs
+++ homework001_maxmin/Program.cs
@@ -7,14 +7,8 @@ int a = -9, b = -3;
 int max = a;
 int min = b;
 
-if (a > max) max = a;
 if (b > max) max = b;
 if (a < max) min = a;
-if (b < max) min = b;
 
-
-Console.Write("max = ");
-Console.WriteLine(max); 
-Console.Write("min = ");
-Console.WriteLine(min); 
- 
+Console.WriteLine ($"max = {max}"); 
+Console.WriteLine ($"min = {min}"); 
