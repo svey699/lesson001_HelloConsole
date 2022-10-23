9a29b95c9ce8ff70933d74fe21e837c2f2d9bab4
Olga <svey699@gmail.com>
2022-10-23 08:34:41 +0500

    получить третье число

---
+++ homework006_get3dig/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.Write("трехзначное значение=: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("третья цифра "+stringNumber[2]);
