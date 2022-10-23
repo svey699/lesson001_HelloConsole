6937569c01393c653d67593eb49e24b9e7d7238e
Olga <svey699@gmail.com>
2022-10-23 08:55:29 +0500

    день недели

---
+++ homework_DayOfWeek/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+string mes;
+Console.WriteLine("День недели: 6, 7, 1");
+string day = Console.ReadLine();
+  switch (day)
+   {  case "6": mes = "да"; break;
+      case "7": mes = "да"; break;
+      default:   mes = "нет"; break;
+   }
+Console.WriteLine(mes);
