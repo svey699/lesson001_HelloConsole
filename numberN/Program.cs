59c29221a7e95352901e63b09531a7120a5bdf0c
Olga <svey699@gmail.com>
2022-10-23 08:03:35 +0500

    с последней задачей

--- numberN/Program.cs
+++ numberN/Program.cs
@@ -2,11 +2,6 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-int [] array = {1, 2, 3, 4, 5};
-int n = array.Length;
-int index = 0;
-while (index < n)
-{
-index = index + 1;
-}
-if (n % 2== 0) Console.WriteLine (n);
+for (int i = 2; i <= 5; i += 2) Console.WriteLine(i);
+for (int i = 2; i <= 8; i += 2) Console.WriteLine(i);
+
