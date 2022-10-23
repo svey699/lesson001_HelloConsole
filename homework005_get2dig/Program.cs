d451989089febc1a96d0d27f19f49c3338dde68b
Olga <svey699@gmail.com>
2022-10-23 08:27:52 +0500

    получить второе число

---
+++ homework005_get2dig/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.Write("трехзначное значение=: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра "+stringNumber[1]);
