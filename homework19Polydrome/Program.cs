61a2505c5e7b6bd0ee6edb5e92a95cb2aa862a41
Olga <svey699@gmail.com>
2022-10-29 20:05:00 +0500

    палиндром

---
+++ homework19Polydrome/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+{
+int[] nums = {1, 2, 8, 2, 1};
+bool number = false;
+for (int i = 0; i < nums.Length; i++)
+if (nums [i] == nums[nums.Length-1])
+	number = true;
+if (number)
+	 Console.WriteLine($"Число: {nums} - является палиндромом.");
+else 
+     Console.WriteLine($"Число: {nums} - не является палиндромом.");
+}
