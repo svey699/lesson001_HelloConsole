5153cba04a9bd5f51e2414cf069baeb2290acb6b
Olga <svey699@gmail.com>
2022-10-30 15:26:16 +0500

    массив из 8 элементов

---
+++ homework29_massiv/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+int[] arr1 = {1, 2, 5, 7, 19};
+int [] arr2 = {6, 1, 33};
+void PrintArray(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write($"{array[i]}");
+}
+Console.WriteLine();
+}
+PrintArray(arr1);
+PrintArray(arr2);
